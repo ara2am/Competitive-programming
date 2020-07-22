02cd0590140148467d0995cdecce668658e118eb
Aradam <12f4e3f48b7e6bc7b@aradam.pp.ua>
2020-07-23 00:00:27 +0300

    sort -> stable_sort.

--- informatics.mccme.ru/Изучение языка программирования/Типы данных/Структуры/K. Отсортировать по среднему баллу/332.cpp
+++ informatics.mccme.ru/Изучение языка программирования/Типы данных/Структуры/K. Отсортировать по среднему баллу/332.cpp
@@ -7,18 +7,12 @@ using namespace std;
 
 struct Student
 {
-	int id;
 	string surname, name;
 	int m;
 };
 
 bool compare (Student s1, Student s2)
 {
-	if (s1.m == s2.m)
-	{
-		return (s1.id < s2.id);
-	}
-
 	return (s1.m > s2.m);
 }
 
@@ -34,10 +28,10 @@ int main ()
 		int a, b, c;
 		cin >> surname >> name >> a >> b >> c;
 
-		students.push_back (Student {i, surname, name, a + b + c});
+		students.push_back (Student {surname, name, a + b + c});
 	}
 
-	sort (students.begin (), students.end (), compare);
+	stable_sort (students.begin (), students.end (), compare);
 
 	for (Student s : students)
 	{
